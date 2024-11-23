b1e1f820fc55841428caee82336beb22b66fb2b5
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-11-23 22:23:13 +0200

    added human input

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 function getComputerChoice() {
     num = Math.random();
     if (num >= 0.6) {
-        return "Rock"
+        return "Rock";
     }
     else if (num >= 0.3) {
-        return "Paper"
+        return "Paper";
     }
     else if (num >= 0) {
-        return "Scissors"
+        return "Scissors";
     }
 
+}
+
+function getHumanChoice() {
+    choice = prompt('Rock, Paper, or Scissors?').toLowerCase().trim();
+
+    if (choice == "rock" || choice == "paper" || choice == "scissors") {
+        return choice;
+    }
+    else
+    {
+        console.log('invalid input, defaulting to rock!');
+        return "rock";
+    }
 }
